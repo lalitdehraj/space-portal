c2f3b738864fe325c70ee11a40cb76950b0af8b5
LalitDehraj <lalitrock97@gmail.com>
2025-09-23 15:53:28 +0530

    changes

--- src/app/api/download/[fileName]/route.ts
+++ src/app/api/download/[fileName]/route.ts
@@ -2,10 +2,7 @@ import { NextRequest } from "next/server";
 import fs from "fs";
 import path from "path";
 
-export async function GET(
-  req: NextRequest,
-  context: { params: Promise<{ fileName: string }> }
-) {
+export async function GET(req: NextRequest, context: { params: Promise<{ fileName: string }> }) {
   const params = await context.params;
   const filePath = path.join(process.cwd(), "reports", params.fileName);
 
@@ -14,7 +11,7 @@ export async function GET(
   }
 
   const fileStream = fs.createReadStream(filePath);
-  return new Response(fileStream as any, {
+  return new Response(fileStream as unknown as ReadableStream, {
     headers: {
       "Content-Type": "application/json",
       "Content-Disposition": `attachment; filename="${params.fileName}"`,
