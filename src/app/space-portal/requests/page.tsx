ef54f9fc0e6bc082abc795a7b55375cb1bb18410
LalitDehraj <lalitrock97@gmail.com>
2025-09-24 12:01:53 +0530

    changes

--- src/app/space-portal/requests/page.tsx
+++ src/app/space-portal/requests/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 import React, { useEffect, useState, useRef } from "react";
 import Pagination from "@/components/PageNumberIndicator";
-import {
-  RoomRequestTable,
-  RoomRequest,
-  AcademicSessions,
-  Building,
-  Room,
-} from "@/types";
+import { RoomRequestTable, RoomRequest, AcademicSessions, Building, Room } from "@/types";
 import { api, callApi } from "@/utils/apiIntercepter";
 import { URL_NOT_FOUND } from "@/constants";
 import { useSelector } from "react-redux";
@@ -51,11 +45,7 @@ const recurrenceStatus: { [key: string]: number } = {
   Quarterly: 5,
 };
 
-function sortData(
-  data: RoomRequest[],
-  key: keyof RoomRequest,
-  sortOrder: sortingTypes
-) {
+function sortData(data: RoomRequest[], key: keyof RoomRequest, sortOrder: sortingTypes) {
   if (sortOrder === "") return data;
   return [...data].sort((a, b) => {
     let valueA: any = a[key];
@@ -90,11 +80,7 @@ function sortData(
   });
 }
 
-function filterData(
-  data: RoomRequest[],
-  filters: { [key: string]: string[] },
-  searchQuery: string
-) {
+function filterData(data: RoomRequest[], filters: { [key: string]: string[] }, searchQuery: string) {
   let filteredBySearch = data.filter((item) => {
     const searchableKeys = Object.values(tableHeadersList);
 
@@ -118,12 +104,8 @@ function filterData(
 export default function RequestApprovalPage() {
   const router = useRouter();
   const [curruntPage, setCurruntPage] = useState(1);
-  const acadmeicYear = useSelector(
-    (state: any) => state.dataState.selectedAcademicYear
-  );
-  const acadmeicSession = useSelector(
-    (state: any) => state.dataState.selectedAcademicSession
-  );
+  const acadmeicYear = useSelector((state: any) => state.dataState.selectedAcademicYear);
+  const acadmeicSession = useSelector((state: any) => state.dataState.selectedAcademicSession);
   const [sortState, setSortState] = useState<sortingTypes>("");
   const [activeHeader, setActiveHeader] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
@@ -136,16 +118,8 @@ export default function RequestApprovalPage() {
   const [pageSize, setPageSize] = useState("10");
 
   useEffect(() => {
-    const listAfterFilters = filterData(
-      requestList,
-      appliedFilters,
-      searchQuery
-    );
-    const sortedList = sortData(
-      listAfterFilters,
-      activeHeader as keyof RoomRequest,
-      sortState
-    );
+    const listAfterFilters = filterData(requestList, appliedFilters, searchQuery);
+    const sortedList = sortData(listAfterFilters, activeHeader as keyof RoomRequest, sortState);
     setFilteredList(sortedList);
   }, [requestList, activeHeader, sortState, appliedFilters, searchQuery]);
 
@@ -157,15 +131,12 @@ export default function RequestApprovalPage() {
       if (!(acadmeicSession && acadmeicSession && pageSize)) return;
       setIsLoadingRequests(true);
       try {
-        let response = await callApi<RoomRequestTable>(
-          `${process.env.NEXT_PUBLIC_GET_REQUEST_LIST}` || URL_NOT_FOUND,
-          {
-            limit: pageSize,
-            offset: curruntPage,
-            acadSess: acadmeicSession,
-            acadYr: acadmeicYear,
-          }
-        );
+        let response = await callApi<RoomRequestTable>(`${process.env.NEXT_PUBLIC_GET_REQUEST_LIST}` || URL_NOT_FOUND, {
+          limit: pageSize,
+          offset: curruntPage,
+          acadSess: acadmeicSession,
+          acadYr: acadmeicYear,
+        });
         console.log(response);
         setTotalPages(parseInt(response.data?.totalPages || "0"));
         setCurruntPage(parseInt(response.data?.curruntPage || "0"));
@@ -182,10 +153,7 @@ export default function RequestApprovalPage() {
   const filterRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (
-        filterRef.current &&
-        !filterRef.current.contains(event.target as Node)
-      ) {
+      if (filterRef.current && !filterRef.current.contains(event.target as Node)) {
         setIsFilterDropdownOpen(false);
       }
     };
@@ -210,9 +178,7 @@ export default function RequestApprovalPage() {
   const handleFilterChange = (filterType: string, value: string) => {
     setAppliedFilters((prevFilters) => {
       const currentOptions = prevFilters[filterType] || [];
-      const newOptions = currentOptions.includes(value)
-        ? currentOptions.filter((item) => item !== value)
-        : [...currentOptions, value];
+      const newOptions = currentOptions.includes(value) ? currentOptions.filter((item) => item !== value) : [...currentOptions, value];
       return {
         ...prevFilters,
         [filterType]: newOptions,
@@ -223,27 +189,19 @@ export default function RequestApprovalPage() {
   const handleRemoveFilter = (filterType: string, value: string) => {
     setAppliedFilters((prevFilters) => {
       const newFilters = { ...prevFilters };
-      newFilters[filterType] = newFilters[filterType].filter(
-        (item) => item !== value
-      );
+      newFilters[filterType] = newFilters[filterType].filter((item) => item !== value);
       return newFilters;
     });
   };
 
   const handleRequestClick = (request: RoomRequest) => {
-    router.push(
-      `/space-portal/requests/${encrypt(
-        request.requestID
-      )}?limit=${pageSize}&offSet=${curruntPage}`
-    );
+    router.push(`/space-portal/requests/${encrypt(request.requestID)}?limit=${pageSize}&offSet=${curruntPage}`);
   };
 
   return (
     <div>
       <div className="flex flex-col md:flex-row justify-between">
-        <h2 className="text-base font-semibold text-gray-800 md:ml-2">
-          Room Request Overview
-        </h2>
+        <h2 className="text-base font-semibold text-gray-800 md:ml-2">Room Request Overview</h2>
         <div className="flex items-center space-x-4">
           <input
             type="text"
@@ -257,11 +215,7 @@ export default function RequestApprovalPage() {
               onClick={() => setIsFilterDropdownOpen(!isFilterDropdownOpen)}
               className="flex flex-row items-center text-gray-600 text-sm bg-gray-100 py-1 px-2 border border-gray-300 rounded-md hover:bg-gray-200 transition-colors duration-150"
             >
-              <img
-                src={"/images/bx-filter-alt.svg"}
-                alt="icon filter"
-                className="h-5 w-5 mr-2 fill-gray-600"
-              />
+              <img src={"/images/bx-filter-alt.svg"} alt="icon filter" className="h-5 w-5 mr-2 fill-gray-600" />
               Filter
               {Object.values(appliedFilters).flat().length > 0 && (
                 <span className="ml-2 bg-orange-500 text-white rounded-full h-5 w-5 flex items-center justify-center text-xs">
@@ -273,19 +227,12 @@ export default function RequestApprovalPage() {
               <div className="absolute right-0 mt-2 w-64 bg-white border border-gray-300 rounded-md shadow-lg p-4 z-10">
                 <div className="space-y-4">
                   <div>
-                    <h4 className="text-sm font-semibold mb-2 text-gray-500">
-                      Priority
-                    </h4>
+                    <h4 className="text-sm font-semibold mb-2 text-gray-500">Priority</h4>
                     {Object.keys(priorityOrder).map((p) => (
-                      <label
-                        key={p}
-                        className="flex items-center text-gray-700"
-                      >
+                      <label key={p} className="flex items-center text-gray-700">
                         <input
                           type="checkbox"
-                          checked={
-                            appliedFilters.priority?.includes(p) || false
-                          }
+                          checked={appliedFilters.priority?.includes(p) || false}
                           onChange={() => handleFilterChange("priority", p)}
                           className="mr-2 rounded text-orange-600"
                         />
@@ -295,14 +242,9 @@ export default function RequestApprovalPage() {
                   </div>
                   <hr className="my-2" />
                   <div>
-                    <h4 className="text-sm font-semibold mb-2 text-gray-500">
-                      Status
-                    </h4>
+                    <h4 className="text-sm font-semibold mb-2 text-gray-500">Status</h4>
                     {Object.keys(statusOrder).map((s) => (
-                      <label
-                        key={s}
-                        className="flex items-center text-gray-700"
-                      >
+                      <label key={s} className="flex items-center text-gray-700">
                         <input
                           type="checkbox"
                           checked={appliedFilters.status?.includes(s) || false}
@@ -323,15 +265,9 @@ export default function RequestApprovalPage() {
       <div className="flex flex-wrap space-x-2 mb-2">
         {Object.entries(appliedFilters).flatMap(([key, values]) =>
           values.map((value) => (
-            <span
-              key={`${key}-${value}`}
-              className="flex items-center bg-gray-200 text-gray-800 text-sm px-3 py-1 rounded-full"
-            >
+            <span key={`${key}-${value}`} className="flex items-center bg-gray-200 text-gray-800 text-sm px-3 py-1 rounded-full">
               {value}
-              <button
-                onClick={() => handleRemoveFilter(key, value)}
-                className="ml-2 text-gray-500 hover:text-gray-900 focus:outline-none"
-              >
+              <button onClick={() => handleRemoveFilter(key, value)} className="ml-2 text-gray-500 hover:text-gray-900 focus:outline-none">
                 &times;
               </button>
             </span>
@@ -345,23 +281,13 @@ export default function RequestApprovalPage() {
             <thead className="bg-gray-50">
               <tr>
                 {Object.keys(tableHeadersList).map((header) => (
-                  <th
-                    key={header}
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider cursor-pointer group"
-                  >
-                    <div
-                      className="flex items-center"
-                      onClick={() => handleSort(header)}
-                    >
+                  <th key={header} scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider cursor-pointer group">
+                    <div className="flex items-center" onClick={() => handleSort(header)}>
                       {header}
                       <span className="ml-2 flex-none rounded text-gray-400 group-hover:visible invisible">
                         <svg
                           className={`h-3 w-3 ${
-                            activeHeader === tableHeadersList[header] &&
-                            sortState === "asc"
-                              ? "fill-orange-400 visible"
-                              : "fill-gray-400"
+                            activeHeader === tableHeadersList[header] && sortState === "asc" ? "fill-orange-400 visible" : "fill-gray-400"
                           } `}
                           aria-hidden="true"
                           name="asc"
@@ -374,10 +300,7 @@ export default function RequestApprovalPage() {
                         </svg>
                         <svg
                           className={` h-3 w-3 ${
-                            activeHeader === tableHeadersList[header] &&
-                            sortState === "desc"
-                              ? "fill-orange-400 visible"
-                              : "fill-gray-400"
+                            activeHeader === tableHeadersList[header] && sortState === "desc" ? "fill-orange-400 visible" : "fill-gray-400"
                           } `}
                           aria-hidden="true"
                           name="desc"
@@ -399,20 +322,8 @@ export default function RequestApprovalPage() {
                 <tr>
                   <td colSpan={Object.keys(tableHeadersList).length}>
                     <div className="flex justify-center items-center h-32">
-                      <svg
-                        className="animate-spin h-12 w-12 text-orange-500"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                      >
-                        <circle
-                          className="opacity-25"
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="currentColor"
-                          strokeWidth="4"
-                        ></circle>
+                      <svg className="animate-spin h-12 w-12 text-orange-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                         <path
                           className="opacity-75"
                           fill="currentColor"
@@ -426,9 +337,7 @@ export default function RequestApprovalPage() {
               ) : filteredList.length === 0 ? (
                 <tr>
                   <td colSpan={Object.keys(tableHeadersList).length}>
-                    <div className="flex justify-center items-center h-32 text-gray-500">
-                      No requests found.
-                    </div>
+                    <div className="flex justify-center items-center h-32 text-gray-500">No requests found.</div>
                   </td>
                 </tr>
               ) : (
@@ -442,18 +351,10 @@ export default function RequestApprovalPage() {
                     >
                       {request.requestID}
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {request.employeeName}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {request.employeeDepartment}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {request.requestedRoomType}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {request.purpose}
-                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{request.employeeName}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{request.employeeDepartment}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{request.requestedRoomType}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{request.purpose}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm">
                       <span
                         className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full
@@ -470,15 +371,11 @@ export default function RequestApprovalPage() {
                         {request.priority}
                       </span>
                     </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{request.requestDate?.split("T")?.[0]}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {request.requestDate}
+                      {`${request.startDate?.split("T")?.[0]} ${request.startDate !== request.endDate ? ` - ${request.endDate?.split("T")?.[0]}` : ""}`}
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {request.startDate}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {`${request.startTime} - ${request.endTime}`}
-                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{`${request.startTime} - ${request.endTime}`}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm">
                       <span
                         className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full
@@ -493,18 +390,10 @@ export default function RequestApprovalPage() {
                         {request.status}
                       </span>
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {request.allocatedRoomID || "N/A"}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {request.approvedBy || "N/A"}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {request.approvalDate || "N/A"}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      {request.recurrence}
-                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{request.allocatedRoomID || "N/A"}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{request.approvedBy || "N/A"}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{request.approvalDate || "N/A"}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{request.recurrence}</td>
                   </tr>
                 ))
               )}
@@ -523,12 +412,7 @@ export default function RequestApprovalPage() {
           </div>
           <div className="mt-2 text-gray-600 flex items-center mr-6">
             <span className="text-sm text-nowrap mr-2">Rows per page:</span>
-            <select
-              onChange={(e) => setPageSize(e.target.value)}
-              className="border rounded border-gray-300 p-1"
-              name="rowscount"
-              id="rows"
-            >
+            <select onChange={(e) => setPageSize(e.target.value)} className="border rounded border-gray-300 p-1" name="rowscount" id="rows">
               <option value="10">10</option>
               <option value="20">20</option>
               <option value="30">30</option>
